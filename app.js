28552ce61f4a2b239e124c16a886cc5f930847fe
Pablo Aucapiña <soluciones.pera@gmail.com>
2024-09-11 16:20:56 -0500

    Added GET /phase-change-diagram

--- app.js
+++ app.js
@@ -40,4 +40,28 @@ app.post('/teapot', (request, response) => {
   return response.status(418).send(`I'm a teapot`);
 });
 
+const phaseChangeData = {
+  10: { specific_volume_liquid: 0.0035, specific_volume_vapor: 0.0035 },
+  20: { specific_volume_liquid: 0.0029, specific_volume_vapor: 0.0041 },
+  30: { specific_volume_liquid: 0.0027, specific_volume_vapor: 0.0045 },
+  40: { specific_volume_liquid: 0.0025, specific_volume_vapor: 0.005 },
+  50: { specific_volume_liquid: 0.0023, specific_volume_vapor: 0.0055 },
+};
+
+app.get('/phase-change-diagram', (request, response) => {
+  const pressure = parseFloat(request.query.pressure);
+  if (!pressure)
+    return response.status(400).json({
+      error: 'El parámetro "presure" es requerido y debe ser un número',
+    });
+
+  const result = phaseChangeData[pressure];
+
+  if (result) response.json(result);
+  else
+    response
+      .status(400)
+      .json({ error: 'No se encontraron datos para la presión solicitada' });
+});
+
 module.exports = app;
